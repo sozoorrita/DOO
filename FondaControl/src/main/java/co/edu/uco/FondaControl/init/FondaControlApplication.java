cbb9c71dfd65b53719959d803dbba680d9d8e14e
Sebastian Zapata Naranjo <sebastian.zapata2452@uco.net.co>
2025-05-15 04:37:43 -0500

    Carga línea base
    carga del proyecto como línea base con el scoffolding de una aplicacion
    web spring boot.

---
+++ FondaControl/src/main/java/co/edu/uco/FondaControl/init/FondaControlApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.FondaControl.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FondaControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FondaControlApplication.class, args);
+	}
+
+}
---
+++ FondaControl/src/test/java/co/edu/uco/FondaControl/init/FondaControlApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.FondaControl.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FondaControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
