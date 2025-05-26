7b57d184df5ee2c7580b1429afd89011312963c6
jhonny <jhonny.benitez5293@uco.net.co>
2025-05-26 11:01:44 -0500

    carga de la connexion a la base de datos

--- backend/src/main/java/co/edu/uco/FondaControl/init/FondaControlApplication.java
+++ backend/src/main/java/co/edu/uco/FondaControl/init/FondaControlApplication.java
@@ -2,13 +2,11 @@ package co.edu.uco.FondaControl.init;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {
-		"co.edu.uco.FondaControl.api",
-		"co.edu.uco.FondaControl.config"  // Solo carga este DAO
-})
+/**
+ * Clase principal que arranca la aplicación Spring Boot.
+ */
+@SpringBootApplication(scanBasePackages = "co.edu.uco.FondaControl")
 public class FondaControlApplication {
 
 	public static void main(String[] args) {
