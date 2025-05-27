be15498391ecb9ebaa1d566d887a4976976aee6a
Alejoqb7 <alejandroqb40@gmail.com>
2025-05-27 00:34:01 -0500

    Anexo de FondaControlApplication.java modificada

--- backend/src/main/java/co/edu/uco/FondaControl/init/FondaControlApplication.java
+++ backend/src/main/java/co/edu/uco/FondaControl/init/FondaControlApplication.java
@@ -2,9 +2,10 @@ package co.edu.uco.FondaControl.init;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "co.edu.uco.FondaControl.config")
 
 public class FondaControlApplication {
 	public static void main(String[] args) {
