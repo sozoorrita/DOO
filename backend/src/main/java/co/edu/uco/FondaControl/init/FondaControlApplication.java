7ddaf5f2fc5077b65cf697d55626e1e92c41a355
Alejoqb7 <alejandroqb40@gmail.com>
2025-05-27 01:17:13 -0500

    Nueva carga de la clase FondaControlApplication

--- backend/src/main/java/co/edu/uco/FondaControl/init/FondaControlApplication.java
+++ backend/src/main/java/co/edu/uco/FondaControl/init/FondaControlApplication.java
@@ -2,11 +2,8 @@ package co.edu.uco.FondaControl.init;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages = "co.edu.uco.FondaControl.config")
-
+@SpringBootApplication(scanBasePackages = { "co.edu.uco.FondaControl.config", "co.edu.uco.FondaControl.api" })
 public class FondaControlApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FondaControlApplication.class, args);
