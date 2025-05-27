0f1a8b42f5fe019ff391b72c7df003ac96a88e45
Sebastian Zapata Naranjo <sebastian.zapata2452@uco.net.co>
2025-05-27 00:25:29 -0500

    Carga para FondaControlApplication

--- backend/src/main/java/co/edu/uco/FondaControl/init/FondaControlApplication.java
+++ backend/src/main/java/co/edu/uco/FondaControl/init/FondaControlApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FondaControlApplication {
 
+public class FondaControlApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FondaControlApplication.class, args);
 	}
